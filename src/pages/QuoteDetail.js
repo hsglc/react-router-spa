3fa6d3048617136b3f3eb45db624feef900b1658
huseyinglc <huseyingulcuu@gmail.com>
2021-05-21 13:58:19 +0300

    with nested route functionality, i utilized to manage different paths to render different pages

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,6 +1,6 @@
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 import Comments from "../components/comments/Comments";
-import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const DUMMY_QUOTES = [
   { id: "q1", author: "Max", text: "Learning React is fun!" },
@@ -12,15 +12,20 @@ const QuoteDetail = () => {
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
-  if(!quote){
-      return <p>No quote found!</p>
+  if (!quote) {
+    return <p>No quote found!</p>;
   }
 
   return (
     <>
-      <section>
-        <HighlightedQuote  text={quote.text} author={quote.author} />
-      </section>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
